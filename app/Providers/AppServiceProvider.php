c865e9876886db2846012707fb200f2e1e54eb52
isaandreanme <zimamarrois@gmail.com>
2024-10-20 19:56:05 +0700

    login url agency dan user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use TomatoPHP\FilamentUsers\Facades\FilamentUser;
 use TomatoPHP\FilamentSettingsHub\Facades\FilamentSettingsHub;
 use TomatoPHP\FilamentSettingsHub\Services\Contracts\SettingHold;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Route;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -41,6 +44,34 @@ class AppServiceProvider extends ServiceProvider
         //         ->description('Name, Logo, Site Profile') // to translate label just use direct translation path like `messages.text.name`
         //         ->group('General') // to translate label just use direct translation path like `messages.text.name`,
         // ]);
-        
+        // Menggunakan hook untuk memeriksa login dan melakukan redirect
+        // Pastikan redirect hanya terjadi setelah login dan bukan pada setiap request
+        Route::middleware(['auth'])->group(function () {
+            Route::get('/home', function () {
+                $user = Auth::user();
+
+                // Cek session apakah redirect sudah dilakukan
+                if (session('has_redirected', false) === false) {
+                    // Jika user is_agency, redirect ke /admin/workers
+                    if ($user->is_agency) {
+                        session(['has_redirected' => true]);  // Set session
+                        return redirect('/admin/workers');
+                    }
+
+                    // Jika user bukan is_admin dan bukan is_agency, redirect ke /admin/proses
+                    if (!$user->is_admin && !$user->is_agency) {
+                        session(['has_redirected' => true]);  // Set session
+                        return redirect('/admin/proses');
+                    }
+
+                    // Jika user adalah is_admin, biarkan ke /admin
+                    session(['has_redirected' => true]);  // Set session
+                    return redirect('/admin');
+                }
+
+                // Jika sudah di-redirect, biarkan mereka tetap di halaman apapun yang diakses
+                return view('home');
+            });
+        });
     }
 }
